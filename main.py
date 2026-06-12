be9a160121e9e7b1eabaa1b5ee25b088d8e2ca68
Shikhar Gupta <shikharjul01@gmail.com>
2026-06-12 12:14:41 +0530

    Data Validation Phase

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 if __name__ == '__main__':
@@ -11,7 +11,12 @@ if __name__ == '__main__':
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiated the data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-        print(data_ingestion_artifact)
+        print(f"Data Ingestion: {data_ingestion_artifact}\n")
+        logging.info("Data Ingestion completed!!!")
+        data_validation_config = DataValidationConfig(training_pipeline_config=TrainingPipelineConfig())
+        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(f"Data validation artifact: {data_validation_artifact}\n")
 
 
     except Exception as e:
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -10,7 +10,7 @@ FILE_NAME: str = "phishingData.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
-
+SCHEMA_FILE_PATH: str = "data_schema/schema.yaml"
 
 DATA_INGESTION_COLLECTION_NAME: str = "phishingData"
 DATA_INGESTION_DATABASE_NAME: str = "NETWORK_DB"
@@ -18,3 +18,9 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
