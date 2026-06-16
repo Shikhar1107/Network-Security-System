a6cf948a3b0a3674abedfc16e8ee79600b950462
Shikhar Gupta <shikharjul01@gmail.com>
2026-06-16 12:15:17 +0530

    Deployment changes

--- app.py
+++ app.py
@@ -71,7 +71,9 @@ async def predict_route(request:Request,file:UploadFile=File(...)):
         print(df['predicted_column'])
         df.to_csv('prediction_output/output.csv')
         table_html = df.to_html(classes='table table-stripped')
-        return templates.TemplateResponse("table.html",{"request":request, "table": table_html})
+        return templates.TemplateResponse(request=request,
+        name="table.html",
+        context={"table": table_html})
 
 
     except Exception as e:
